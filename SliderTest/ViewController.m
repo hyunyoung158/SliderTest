06ceb564a9b2ed72dbd2a7b19f230880ea93a0a1
hyunyoung158 <setarong158@gmail.com>
2014-01-03 15:24:17 +0900

    슬라이더 테스트 프로젝트를 업로드했습니다.

---
+++ SliderTest/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  SliderTest
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
+
+@implementation ViewController
+
+- (IBAction)progressChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    self.label.text = [NSString stringWithFormat:@"value: %f", slider.value];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
